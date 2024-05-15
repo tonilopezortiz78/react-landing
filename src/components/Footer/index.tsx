2d32dcdef25918f8c9f922824c322a84273643c0
Tony Lopez <tonilopezortiz78@yahoo.com>
2024-05-15 14:10:47 +0700

    add footer social media links and motivation sentence

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,16 +10,19 @@ const Footer = () => {
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
               <div className="mb-12 max-w-[360px] lg:mb-16">
-                <Link href="/" className="mb-8 inline-block">
+                <Link
+                  href="https://www.youtube.com/@transcendmoney"
+                  className="mb-8 inline-block"
+                >
                   <Image
-                    src="/images/logo/logo-2.svg"
+                    src="/images/logo/mexc.svg"
                     alt="logo"
                     className="w-full dark:hidden"
                     width={140}
                     height={30}
                   />
                   <Image
-                    src="/images/logo/logo.svg"
+                    src="/images/logo/mexc.svg"
                     alt="logo"
                     className="hidden w-full dark:block"
                     width={140}
@@ -27,13 +30,13 @@ const Footer = () => {
                   />
                 </Link>
                 <p className="mb-9 text-base leading-relaxed text-body-color dark:text-body-color-dark">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer lobortis.
+                  Empower your trades Conquer the markets Precision data,
+                  powerful strategies. Fuel your crypto success.
                 </p>
                 <div className="flex items-center">
                   <a
                     href="/"
-                    aria-label="social-link"
+                    aria-label="facebook"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
@@ -52,8 +55,8 @@ const Footer = () => {
                     </svg>
                   </a>
                   <a
-                    href="/"
-                    aria-label="social-link"
+                    href="https://x.com/transcendmoneyt?t=EG-IuqUYhlMNFRS1CKRCxg&s=09"
+                    aria-label="twitter"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
@@ -74,8 +77,8 @@ const Footer = () => {
                     </svg>
                   </a>
                   <a
-                    href="/"
-                    aria-label="social-link"
+                    href="https://www.youtube.com/@transcendmoney"
+                    aria-label="youtube"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="mr-6 text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
@@ -215,23 +218,13 @@ const Footer = () => {
           <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
           <div className="py-8">
             <p className="text-center text-base text-body-color dark:text-white">
-              Template by{" "}
               <a
-                href="http://uideck.com"
+                href="https://www.youtube.com/@transcendmoney"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:text-primary"
               >
-                UIdeck
-              </a>{" "}
-              and{" "}
-              <a
-                href="https://nextjstemplates.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-primary"
-              >
-                Next.js Templates
+                © 2024. Transcend Money Community, All rights reserved.
               </a>
             </p>
           </div>
